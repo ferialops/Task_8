7ac57662150acff8d0b42bc607d4268f4e51dbe0
ferialops <90615135+ferialops@users.noreply.github.com>
2022-01-10 04:07:15 +0300

    Update ConsoleMain.java

--- src/ru/vsu/cs/kunakhova_a_y/ConsoleMain.java
+++ src/ru/vsu/cs/kunakhova_a_y/ConsoleMain.java
@@ -71,9 +71,8 @@ public class ConsoleMain {
 
             for (int i = 0; i < result.length; i++) {
                 System.out.print(result[i]);
-                System.out.print(" ");
+                System.out.print("; ");
             }
-            System.out.println(";");
 
             ArrayUtils.writeArrayToFile(params.outputResult, result);
         }
